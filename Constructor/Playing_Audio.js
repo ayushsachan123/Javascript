aed4662f927b87fac01c0451e7cadfb2c964e2b0
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-11-27 17:50:39 +0530

    Create Playing_Audio.js

---
+++ Constructor/Playing_Audio.js
@@ -0,0 +1,13 @@
+function Audio (fileLooction){
+    this.fileLocation=fileLoction;
+    this.play= function(){
+        //playing
+        var tom3 = new Audio('contructor/tom-1.mp3');
+        tom3.play();
+    }
+}
+
+// const [audio] = useState(new Audio('contructor/tom-1.mp3'));
+// audio.play()
+// this.play();
+
